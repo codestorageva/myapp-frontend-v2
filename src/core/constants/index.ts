92cfc2dc90320375e072a4debe309bea8f778bc1
Vaistra Codestorage <codestorage.vaistra@gmail.com>
2026-04-02 17:07:24 +0530

    Update index.ts

--- src/core/constants/index.ts
+++ src/core/constants/index.ts
@@ -1,7 +1,1 @@
-// ? Write Every Constants related to Our Overall App Here
-// export const SERVER_URL = "http://192.168.1.7:8081";
-export const SERVER_URL = process.env.NEXT_PUBLIC_NEXT_BACKEND_URL || "http://localhost";
-// export const SERVER_URL = 'https://1247-103-167-52-6.ngrok-free.app';
-// export const SERVER_URL = 'https://6d17-103-167-52-6.ngrok-free.app';
-
-        
+export const SERVER_URL = process.env.NEXT_PUBLIC_API_URL || "";

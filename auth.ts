fba2b229dedf7e0ee16cd68f38ee3b5a1bf68dbc
Vaistra Codestorage <codestorage.vaistra@gmail.com>
2026-04-02 16:36:50 +0530

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -1,5 +1,5 @@
 import CredentialsProvider from "next-auth/providers/credentials";
-  import  type {NextAuthConfig}  from "next-auth";
+import type { NextAuthConfig } from "next-auth";
 import NextAuth from "next-auth";
 import { SERVER_URL } from "@/core/constants";
 import { API_ENDPOINTS } from "@/core/constants/api_endpoint";
@@ -11,7 +11,7 @@ interface Credentials {
 
 async function login(credentials: Credentials) {
   try {
-     const response = await fetch(`${SERVER_URL}${API_ENDPOINTS.login}`, {  // append /auth/login directly
+    const response = await fetch(`${SERVER_URL}${API_ENDPOINTS.login}`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -23,18 +23,22 @@ async function login(credentials: Credentials) {
     const data = await response.json();
     console.log("API login response:", data);
 
-    if (data.success) return data;
-    throw new Error(data.message || "Login failed");
+    if (response.ok && data.success) {
+      return data;
+    }
+
+    return null;
   } catch (error: any) {
-    throw new Error(error.message);
+    console.error("LOGIN API ERROR:", error?.message);
+    return null;
   }
 }
 
-export const authOptions  = {
+export const authOptions = {
   session: {
     strategy: "jwt",
   },
-  secret: "thisissecret", 
+  secret: "thisissecret",
   trustHost: true,
   pages: {
     signIn: "/login",
@@ -44,12 +48,13 @@ export const authOptions  = {
       name: "credentials",
       credentials: {},
       async authorize(credentials) {
-        try {
-          const user = await login(credentials as Credentials);          
+        const user = await login(credentials as Credentials);
+
+        if (user) {
           return user;
-        } catch (error) {
-          throw new Error("Failed to login");
         }
+
+        return null;
       },
     }),
   ],
@@ -65,21 +70,20 @@ export const authOptions  = {
         ...session.user,
         success: token.success as boolean,
         successCode: token.successCode as string,
-        email:token.email as string,
+        email: token.email as string,
         roleId: token.roleId as number,
         roleName: token.roleName as string,
         fullName: token.fullName as string,
         userName: token.userName as string,
         mobNo: token.mobNo as string,
-        authToken: token.authToken as string
+        authToken: token.authToken as string,
       };
 
       return session;
     },
   },
-} satisfies NextAuthConfig ;
+} satisfies NextAuthConfig;
 
-
-const { handlers, auth,signOut } = NextAuth(authOptions);
+const { handlers, auth, signOut } = NextAuth(authOptions);
 
 export { handlers, auth, signOut };
